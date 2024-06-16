3593187c58f18529c239c43aac51d6f41a099a23
ZxStim <thang.victor.luong@gmail.com>
2024-06-16 18:50:10 +0700

    edit home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import AirdropErc20Card from "@/components/airdrop-erc20-card";
-import AirdropKaiaCard from "@/components/airdrop-kaia-card";
+import AirdropNativeCard from "@/components/airdrop-native-card";
 import AirdropNftsCard from "@/components/airdrop-nfts-card";
 
 export default function Home() {
@@ -12,7 +12,7 @@ export default function Home() {
         <p className="leading-7 [&:not(:first-child)]:mt-6">A simple tool to airdrop on Kaia chain</p>
       </div>
       <div className="flex flex-col gap-8">
-        <AirdropKaiaCard />
+        <AirdropNativeCard />
         <AirdropErc20Card />
         <AirdropNftsCard />
       </div>
