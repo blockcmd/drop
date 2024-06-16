1b9715a9ca417870a3891cb5353d4167a41b1ca2
ZxStim <thang.victor.luong@gmail.com>
2024-06-16 18:49:45 +0700

    edit native card

--- components/airdrop-native-card.tsx
+++ components/airdrop-native-card.tsx
@@ -1,12 +1,12 @@
 import Link from 'next/link';
 import { ArrowRight } from 'lucide-react';
 
-export default function AirdropKaiaCard() {
+export default function AirdropNativeCard() {
   return (
     <Link href="/airdrop-kaia">
       <div className="flex flex-row justify-between items-center shadow-lg w-full p-8 border-2 border-secondary hover:border-primary">
         <div className='flex flex-col gap-4'>
-          <h2 className="scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0">Airdrop $KAIA</h2>
+          <h2 className="scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0">Airdrop Native</h2>
           <p>Go to deploy page</p>
         </div>
         <ArrowRight className="w-8 h-8" />
